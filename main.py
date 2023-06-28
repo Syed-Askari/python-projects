aa4558057520f117c98859e0aae651caa3f6fecf
Syed Askari <s.askari1214@gmail.com>
2023-06-28 07:59:28 +0000

    First commmit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 
 def get_driver():
@@ -7,16 +8,25 @@ def get_driver():
   options.add_argument("start-maximized")
   options.add_argument("disable-dev-shm-usage")
   options.add_argument("no-sandbox")
-  options.add_experimental_option("excludeSwitches",["enable-automation"])
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
   driver = webdriver.Chrome(options=options)
   driver.get("https://automated.pythonanywhere.com")
   return driver
 
+
+def remove_text(text):
+  txt = text.split(": ")
+  return txt[1]
+
+
 def main():
+
   driver = get_driver()
-  element  = driver.find_element(by="xpath",value = "/html/body/div[1]/div/h1[1]")
-  return element.text
+  time.sleep(3)
+  element = driver.find_element(by="xpath",
+                                value="/html/body/div[1]/div/h1[2]")
+  return remove_text(element.text)
 
 
 print(main())
